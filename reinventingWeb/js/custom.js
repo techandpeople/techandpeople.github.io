8b1f5317e2e1c98fa01c051429d3dac8285f2948
Sérgio Alves <sergioalves.194@gmail.com>
2021-03-19 10:24:59 +0000

    reinventing the web->responsive

--- reinventingWeb/js/custom.js
+++ reinventingWeb/js/custom.js
@@ -23,6 +23,7 @@
 
 
     // SLIDER
+    
     $('.owl-carousel').owlCarousel({
       animateOut: 'fadeOut',
       items:1,
@@ -34,6 +35,7 @@
     pullDrag: false,
     touchDrag: false,
     })
+    
 
 
     // PARALLAX EFFECT
@@ -51,19 +53,22 @@
           enabled:true
         },
         zoom: {
-        enabled: true, // By default it's false, so don't forget to enable it
+          enabled: true, // By default it's false, so don't forget to enable it
 
-        duration: 300, // duration of the effect, in milliseconds
-        easing: 'ease-in-out', // CSS transition easing function
+          duration: 300, // duration of the effect, in milliseconds
+          easing: 'ease-in-out', // CSS transition easing function
 
-        // The "opener" function should return the element from which popup will be zoomed in
-        // and to which popup will be scaled down
-        // By defailt it looks for an image tag:
-        opener: function(openerElement) {
-        // openerElement is the element on which popup was initialized, in this case its <a> tag
-        // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-        return openerElement.is('img') ? openerElement : openerElement.find('img');
-        }
+          // The "opener" function should return the element from which popup will be zoomed in
+          // and to which popup will be scaled down
+          // By defailt it looks for an image tag:
+          opener: function(openerElement) {
+            // openerElement is the element on which popup was initialized, in this case its <a> tag
+            // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+            return openerElement.is('img') ? openerElement : openerElement.find('img');
+          }
+       },
+       image: {
+        titleSrc: 'desc'
       }
     });  
 
